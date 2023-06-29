a7d0164f265df5d1f12c4ff0ecf7304615e41974
Prigozev-Yuriy <138002979+Prigozev-Yuriy@users.noreply.github.com>
2023-06-29 17:26:57 +0300

    first commit

---
+++ Homework/Task_10/Program.cs
@@ -0,0 +1,9 @@
+﻿// Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает 
+// вторую цифру этого числа.
+// 456 -> 5
+// 782 -> 8
+// 918 -> 1
+Console.Write("Введите цифру: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int secondDigit = (number - number%10)/10 - (number/100)*10;
+Console.WriteLine($"Вторая цифра в числе {number} является {secondDigit}");
