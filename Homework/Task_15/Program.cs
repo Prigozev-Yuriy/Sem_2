af3814fa89d4ee99c06dee0b8897516ee19d10a0
Prigozev-Yuriy <138002979+Prigozev-Yuriy@users.noreply.github.com>
2023-06-29 19:54:10 +0300

    CommitTask_15

--- Homework/Task_15/Program.cs
+++ Homework/Task_15/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Write("Введите цифру: ");
+int number = Convert.ToInt32(Console.ReadLine());
+if (number > 7)
+{
+    Console.WriteLine("Вы ввели число, не соответсвующее дню недели");
+}
+else
+{
+    if (number == 6 ^ number == 7)
+    {
+        Console.WriteLine($"Этот день является выходным днем!");
+    }
+    else
+        Console.WriteLine("Этот день является рабочим днем!");
+}
