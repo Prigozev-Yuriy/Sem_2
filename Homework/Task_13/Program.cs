28ff483222f23a0c1413dfa47e23c937ed1c7da9
Prigozev-Yuriy <138002979+Prigozev-Yuriy@users.noreply.github.com>
2023-06-29 19:40:40 +0300

    CommitTask_13

---
+++ Homework/Task_13/Program.cs
@@ -0,0 +1,19 @@
+﻿// Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет. До 10 символов
+// 645 -> 5
+// 78 -> третьей цифры нет
+// 32679 -> 6
+Console.Write("Введите число до 10 символов: ");
+string? number = Console.ReadLine();
+if (number.Length > 10)
+{
+    Console.WriteLine("Ведено слишком большое число!");
+}
+else
+{
+    if (number.Length < 3)
+    {
+        Console.WriteLine("Третьей цифры нет");
+    }
+    else
+        Console.WriteLine($"Третья цифра в числе {number} является {number.ToString()[2]} ");
+}
